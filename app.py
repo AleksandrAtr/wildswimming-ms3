ae77b0d2d387ab9806046b0be874da188842f72b
AleksandrAtr <atraleks@gmail.com>
2024-01-05 09:51:00 +0000

    add: Posts.html template

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World Swimmers"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/get_posts")
+def get_posts():
+    categories = mongo.db.categories.find()
+    return render_template("posts.html", categories=categories)
 
 
 if __name__ == "__main__":
