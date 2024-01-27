ba45f5bc5af19b5858b913807469d366fbc3e2fb
AleksandrAtr <atraleks@gmail.com>
2024-01-27 11:08:48 +0000

    add: redirect to the same page functionality

--- app.py
+++ app.py
@@ -24,8 +24,36 @@ def home():
     """
 
     """
-    img_url = url_for('static', filename='images/cover.jpg')
+    img_url = url_for("static", filename="images/cover.jpg")
     return render_template("index.html", img_url=img_url)
+
+
+@app.route("/url_redirect/<template>", methods=["GET", "POST"])
+def url_redirect(template):
+    if template == "blog":
+        if "user" in session:
+            return render_template("create_post.html")
+        else:
+            flash("Log in to post", "error")
+            session["blog_url"] = request.url
+            return redirect(url_for("login"))
+    return "Template Link argument does not match with this specific route", 404 
+
+
+@app.route("/url_edit/<post_id>/<template>", methods=["GET", "POST"])
+def url_edit(post_id, template):
+    if template == "edit_profile":
+        session["edit_profile"] = request.url
+        return redirect(url_for("edit_post", post_id=post_id))
+    return "Template Link argument does not match with this specific route", 404 
+
+
+@app.route("/url_delete/<post_id>/<template>", methods=["GET", "POST"])
+def url_delete(post_id, template):
+    if template == "delete_profile":
+        session["delete_profile"] = request.url
+        return redirect(url_for("delete_post", post_id=post_id))
+    return "Template Link argument does not match with this specific route", 404 
 
 
 @app.route("/get_posts")
@@ -40,8 +68,8 @@ def get_posts():
     Returns:
     str: Rendered HTML content for the 'blog.html' template.
     """
-    post= list(mongo.db.posts.find())
-    image_url = url_for('static', filename='images/post_default.jpg')
+    post = list(mongo.db.posts.find())
+    image_url = url_for("static", filename="images/post_default.jpg")
     return render_template("blog.html", post=post, image_url=image_url)
 
 
@@ -49,7 +77,7 @@ def get_posts():
 def search():
     query = request.form.get("query")
     get_posts = list(mongo.db.posts.find({"$text": {"$search": query}}))
-    image_url = url_for('static', filename='images/post_default.jpg')
+    image_url = url_for("static", filename="images/post_default.jpg")
     return render_template("blog.html", post=get_posts, image_url=image_url)
 
 
@@ -73,40 +101,32 @@ def create_post():
     """
     if request.method == "POST":
         # get user input and user details
-        title = request.form['title']
-        content = request.form['content']
-        keywords = [tag.strip() for tag in request.form['keywords'].split(',') 
+        title = request.form["title"]
+        content = request.form["content"]
+        keywords = [tag.strip() for tag in request.form["keywords"].split(",") 
                     if tag.strip()]
         # verify title input
         if not title or len(title) > 50:
             flash("Title shall not be more than 50", "error")
-            return render_template('create_post.html', title_input=title, 
+            return render_template("create_post.html", title_input=title, 
                                    textarea_content=content)
         # verify content input
         if not content or len(content) > 1000:
             flash("Post content shall not be more than 1000", "error")
-            return render_template('create_post.html', title_input=title, 
+            return render_template("create_post.html", title_input=title, 
                                    textarea_content=content)
-
         # get user details
         user_details = mongo.db.users.find_one({"username": session["user"]})
         # assign user details to author
         author = {
-            'id': str(user_details["_id"]),
-            'username': user_details["username"]
+            "id": str(user_details["_id"]),
+            "username": user_details["username"]
         }
         # create a date stamp with a specific format
-        date_stamp = datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S')
+        date_stamp = datetime.utcnow().strftime("%d-%m-%Y %H:%M:%S")
         # 
         return save_post(title, content, author, keywords, date_stamp)
-
-    # if  method is GET check if user in session if not redirect to log in
-    if "user" in session:
-        return render_template("create_post.html")
-    else:
-        flash("Log in to post", "error")
-        return redirect(url_for("login"))
-
+    return render_template("create_post.html")
 
 # save post to mongoDB function
 def save_post(title, content, author, keywords, date_stamp):
@@ -127,11 +147,11 @@ def save_post(title, content, author, keywords, date_stamp):
     str: Redirects to the 'get_posts' route after successfully saving the post.
     """
     post_data = {
-        'title': title,
-        'content': content,
-        'author': author,
-        'keywords': keywords,
-        'created_at': date_stamp
+        "title": title,
+        "content": content,
+        "author": author,
+        "keywords": keywords,
+        "created_at": date_stamp
     }
     # insert the post into the MongoDB collection
     mongo.db.posts.insert_one(post_data)
@@ -139,12 +159,13 @@ def save_post(title, content, author, keywords, date_stamp):
     return redirect(url_for("get_posts"))
 
 
+
 @app.route("/edit_post/<post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     if request.method == "POST":
-        title = request.form.get('title')
-        content = request.form.get('content')
-        keywords = [tag.strip() for tag in request.form['keywords'].split(',') 
+        title = request.form.get("title")
+        content = request.form.get("content")
+        keywords = [tag.strip() for tag in request.form["keywords"].split(",") 
                     if tag.strip()]
         # verify title input
         if not title or len(title) > 50:
@@ -162,6 +183,10 @@ def edit_post(post_id):
             {"$set": {"title": title, "content": content, 
                       "keywords": keywords}})
         flash("Post updated", "success")
+        # redirection use check
+        edit_profile= session.pop("edit_profile", None)
+        if edit_profile:
+            return redirect(url_for("profile"))
         return redirect(url_for("get_posts"))
     # get and check if post exists in database
     post = mongo.db.posts.find_one({"_id": ObjectId(post_id)})
@@ -179,7 +204,10 @@ def delete_post(post_id):
         print(post)
         mongo.db.posts.delete_one(post)
         flash("Post deleted", "success")
-        return redirect(url_for('get_posts'))
+        delete_profile= session.pop("delete_profile", None)
+        if delete_profile:
+            return redirect(url_for("profile"))
+        return redirect(url_for("get_posts"))
     else:
         return "Post not found", 404
 
@@ -244,17 +272,24 @@ def login():
     """
     if request.method == "POST":
         # Assign form inputs to new variables
-        username = request.form.get('username').lower()
-        password = request.form.get('password')
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
 
-        user_id = mongo.db.users.find_one({'username': username})
-
-        if user_id and check_password_hash(user_id['password'], password):
-            flash('Login successful!', 'success')
+        user = mongo.db.users.find_one({'username': username})
+        # check if entered details match database
+        if user and check_password_hash(user['password'], password):
+            flash("Login successful!", "success")
+            # create user session
             session["user"] = request.form.get("username").lower()
+            # check if redirection session exists
+            blog_url = session.pop("blog_url", None)
+            # redirect base on the url session
+            if blog_url:
+                # print("I am login if prior redirection: " + session["blog_url"])
+                return redirect(url_for("create_post"))
             return redirect(url_for("profile"))
         else:
-            flash('Invalid username or password. Please try again.', 'error')
+            flash("Invalid username or password. Please try again.", "error")
     return render_template("login.html")
 
 
@@ -293,7 +328,8 @@ def logout():
     Returns:
     str: Redirects to the 'login' route after logging out.
     """
-    session.pop('user', None)
+    for key in list(session.keys()):
+        session.pop(key, None)
     flash('You have logged out', 'success')
     return redirect(url_for('login'))
 
