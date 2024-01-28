6ebcc4a67136809055e36f91c9b7752a67d16e2b
AleksandrAtr <atraleks@gmail.com>
2024-01-28 09:37:43 +0000

    fix: add comments and structure

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
+/* jshint esversion: 8, jquery: true, scripturl: true */
 
-
+  // jQuery for MaterializeCSS initialization
   $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('input#title, textarea#content').characterCounter();
     $('.modal').modal();
-    $('.collapsible').collapsible();
   });
     
 
