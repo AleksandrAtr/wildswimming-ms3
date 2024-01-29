06233f1214e7d18b3e7868c66fc69caefc8eb5e7
AleksandrAtr <atraleks@gmail.com>
2024-01-29 22:25:27 +0000

    add: Delete post functionality

--- static/js/script.js
+++ static/js/script.js
@@ -6,5 +6,17 @@
     $('input#title, textarea#content').characterCounter();
     $('.modal').modal();
   });
-    
 
+  // delete post functionality
+    document.addEventListener('DOMContentLoaded', function() {
+      var myButton = document.getElementById('modal-delete');
+      
+      if (myButton) {
+          myButton.addEventListener('click', function() {
+              // get post object id assigned to the variable
+              var objectId = this.getAttribute('post-id');
+              // assign new link to the Delete post anchor
+              document.getElementById('delete-post').href = "/delete_post/"+objectId;
+          });
+      }
+  });
